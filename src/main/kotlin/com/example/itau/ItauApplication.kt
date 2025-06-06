bb55c593fefde4184849f19008d4af061f63d469
Sakuma <yudisakuma@gmail.com>
2025-06-06 03:39:06 -0300

    created spring project

---
+++ src/main/kotlin/com/example/itau/ItauApplication.kt
@@ -0,0 +1,11 @@
+package com.example.itau
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItauApplication
+
+fun main(args: Array<String>) {
+	runApplication<ItauApplication>(*args)
+}
